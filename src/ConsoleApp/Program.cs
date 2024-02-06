d24a5e99d655fd8d361d9c5d5a1c14ba00b4fe2f
Romaniec Jakub <jakub.romaniec@outlook.com>
2024-02-06 17:31:41 +0100

    Exemplary IConfiguration support

--- src/ConsoleApp/Program.cs
+++ src/ConsoleApp/Program.cs
@@ -2,15 +2,18 @@
 
 
 using ConsoleApp;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
+//To set value via EnvVar
+//Environment.SetEnvironmentVariable("EXAMPLE__ConfigName", "MyExecOverwrittenValue");
 
 using var host = CreateHostBuilder(args).Build();
 await host.StartAsync();
 
 var sampleService = host.Services.GetRequiredService<ISampleService>();
-
 await sampleService.DisplayAsync("Hello world!");
 
 Console.WriteLine("Press any key to continue");
@@ -18,13 +21,21 @@ Console.ReadKey();
 
 static IHostBuilder CreateHostBuilder(string[] args) =>
     Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((context, configBuilder) =>
+        {
+            configBuilder
+                .AddJsonFile("appSettings.json", optional: false)
+                .AddJsonFile($"appSettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables("EXAMPLE:"); //https://learn.microsoft.com/en-us/dotnet/core/compatibility/extensions/7.0/environment-variable-prefixn
+        })
         .UseConsoleLifetime()
         .ConfigureLogging((_, builder) => builder
             .AddConsole()
             .AddFilter("Microsoft.Hosting", LogLevel.Warning)
             .AddFilter("System.Net.Http", LogLevel.Warning)
             .SetMinimumLevel(LogLevel.Information))
-        .ConfigureServices((_, services) =>
+        .ConfigureServices((context, services) =>
         {
             services.AddSingleton<ISampleService, SampleService>();
+            services.Configure<Configuration>(context.Configuration);
         });
--- src/ConsoleApp/SampleService.cs
+++ src/ConsoleApp/SampleService.cs
@@ -2,14 +2,16 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 
 namespace ConsoleApp;
 
-internal class SampleService(ILogger<SampleService> logger) : ISampleService
+internal class SampleService(ILogger<SampleService> logger, IOptions<Configuration> config) : ISampleService
 {
     public Task DisplayAsync(string message)
     {
         logger.LogInformation("Message to display: {message}",message);
+        logger.LogInformation("Configuration {configKey}: {configValue}", nameof(Configuration.ConfigName), config.Value.ConfigName);
         return Task.CompletedTask;
     }
 }
